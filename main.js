2ded3f6d410cded30d4f7717ad815d94196e5078
rebecca-mckeever <rebecca.mckeever@g.austincc.edu>
2021-11-04 04:03:44 -0500

    added function to convert meters to miles

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 var https = require('https');
 
+let MI_TO_M = 1609.344;     // number of meters in a mile
+
+// Convert the given value in meters to miles, rounded to the 
+// same number of sig figs.
+function get_miles(meters) {
+    var meters_string = meters.toString();
+    var sig_figs = 0;
+    
+    // For each character of the string version of meters, increment
+    // sig figs if the character is a numeric value.
+    const regex = new RegExp('[0-9]');
+    for (const ch of meters_string) {
+        sig_figs += (regex.test(ch)) ? 1 : 0;
+    }
+    return Number.parseFloat((meters/MI_TO_M).toPrecision(sig_figs));
+}
+
 var url = "https://api.openweathermap.org/data/2.5/weather?";
 
 let params =
